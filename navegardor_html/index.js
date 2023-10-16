8c840153de576a8230243a6c3551b3ba3d381c48
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-16 20:45:28 -0300

    js

---
+++ navegardor_html/index.js
@@ -0,0 +1,6 @@
+//comentario
+console.log('BOA NOITE');
+console.log('Este trecho será exibido no console do navegador')
+alert('PROIBIDO ENTRAR') //aparece na pagina!
+
+// NO console da pagina posso colocar um 'alert('mensagem')'
---
+++ variaveis/let.js
@@ -0,0 +1,24 @@
+let nome = 'João'; //String
+let nome2 = 'leonardo'
+
+let nome3; //declarei uma variavel e posso chamar ela
+nome3 = 'Chamei ela';
+console.log(nome3);
+nome3 = 'chamei ela parte 2';
+console.log(nome3);
+// var nomee = 'João';
+
+
+console.log(nome,'nasceu em 2004');
+console.log('em 200', nome, 'conheceu maria');
+console.log(nome,'corre muito');
+console.log(nome,'nasceu e' ,nome2 ,'nasceu e em 2004');
+
+//camelCase
+
+let nomeCompletoDoCliente = 'Leonardo Feitosa';
+console.log(nomeCompletoDoCliente)
+
+let nomeCompleto = 'leonardo feitosa'
+let nomecompleto = 'barroso'
+console.log(nomeCompleto, nomecompleto)
