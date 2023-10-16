23e010ff93cf4b5c83303252bd9689137698b5fd
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-16 20:54:47 -0300

    constate.js

---
+++ variaveis/constates/constate.js
@@ -0,0 +1,15 @@
+const nome = 'Leonardo';
+//nao pode modificar o valor ''nome = feitosa''
+console.log(nome)
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const resultado = primeiroNumero * segundoNumero;
+
+const resultadoDuplicado = resultado * 2;
+
+let resultadoTriplicado = resultadoDuplicado * 3;
+
+console.log(resultadoDuplicado);
+
+console.log(typeof primeiroNumero)
