cdf8e0e4d490977e2c6b96c37d758866ff24a619
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-16 21:28:53 -0300

    exercicio.js

---
+++ variaveis/exec.js
@@ -0,0 +1,11 @@
+const nome = 'Leonardo Feitosa';
+const sobrenome = 'Barroso';
+const idade = 19;
+const peso = 69;
+const alturaEmM = 1.75
+const imc = peso / (alturaEmM * alturaEmM)
+const anoNascimento = 2023 - idade
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg` )
+console.log(`tem ${alturaEmM} e seu IMC é de ${imc}`)
+console.log(`${nome} nasceu em ${anoNascimento}`)
