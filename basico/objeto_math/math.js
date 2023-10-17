b4b70572f84e24f3928a265cc2b25061f221f734
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-17 15:48:54 -0300

    summary

---
+++ basico/objeto_math/math.js
@@ -0,0 +1,9 @@
+console.log(!!(100 / 0));
+
+// let num2 = Math.floor(num1); arredonda para baixo
+// let num2 = Math.ceil(num1); arredonda para cima
+// let num2 = Math.round(num1);  arredondar para o mais proximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); o maior numero da sequencia
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); o menor numero da sequencia
+// const aleatorio = Math.round(Math.random() ; math.round vai chegar um numero aleatorio entre 0 e 1 mas 1 nunca irá aparecer
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); vai ficar numeros aleatorios entre 10 e 5
--- basico/objeto_math/number/number.js
+++ basico/objeto_math/number/number.js
@@ -2,9 +2,9 @@
 let num1 = 0.7; // number
 let num2 = 0.1; // number
 
-// num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
-// num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.9
-// num1 = ((num1 * 100) + (num2 * 100)) / 100; // 1.0
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.9
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 1.0
 
 num1 = Number(num1.toFixed(2)); 
 
