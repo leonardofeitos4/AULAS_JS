ceab32bfd343d3115ddc05901ce4bb3c697997d6
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-16 22:14:51 -0300

    JS

---
+++ basico/aritimetrico.js
@@ -0,0 +1,14 @@
+
+let contador = 1;
+contador++;
+contador++;
+contador++;
+contador++;
+console.log(contador)
+
+let contador2 = 1;
+console.log(++contador2)
+
+let contador3 = 1;
+// contador3++; ou contador3--
+console.log(contador3++) // ver qual o valor do CONTADOR3 
