b85c0fcaa08f3b632ef923f692c587d67e088550
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-16 21:30:18 -0300

    Update exec.js

--- variaveis/exec.js
+++ variaveis/exec.js
@@ -2,10 +2,10 @@ const nome = 'Leonardo Feitosa';
 const sobrenome = 'Barroso';
 const idade = 19;
 const peso = 69;
-const alturaEmM = 1.75
-const imc = peso / (alturaEmM * alturaEmM)
-const anoNascimento = 2023 - idade
+const alturaEmM = 1.75;
+const imc = peso / (alturaEmM * alturaEmM);
+const anoNascimento = 2023 - idade;
 
-console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg` )
-console.log(`tem ${alturaEmM} e seu IMC é de ${imc}`)
-console.log(`${nome} nasceu em ${anoNascimento}`)
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg` );
+console.log(`tem ${alturaEmM} e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
