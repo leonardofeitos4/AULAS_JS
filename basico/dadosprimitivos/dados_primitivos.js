64cf32d6196e182b54bae802729a0bc24faae753
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-16 22:09:05 -0300

    js

---
+++ basico/dadosprimitivos/dados_primitivos.js
@@ -0,0 +1,16 @@
+const nomee = 'Leonardo';
+const nome1 = "leonardo";
+const nome2 = `leonardo`;
+const num1 = 10;
+const nume2 = 10.50;
+let nomeAluno;
+let sobrenomeAluno = null; //Nulo aponta para local nenhuma na memoria
+const aprovado = false;
+const boolean = true;
+
+let a = 2
+const b = a
+console.log(a, b)
+
+a = 3
+console.log(a,b)
