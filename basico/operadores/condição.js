ad60ef5175d1dea7d41d893d2e939b45c05f3f71
Leonardo  Feitosa <leonardofeitos4@gmail.com>
2023-10-18 17:41:53 -0300

    commit

---
+++ basico/operadores/condição.js
@@ -0,0 +1,21 @@
+/*
+&& -> false && true -> false "o valor mesmo"
+|| -> true && false -> vai retornar "o valor verdadeiro"
+*/
+
+function falarOi() {
+
+    return 'oi';
+
+}
+
+const vaiExecutar = 'leonardo';
+
+console.log(vaiExecutar && falarOi()) 
+
+console.log( 0 || false || 'leonardo' || true) //retorna o primeiro valor verdadeiro
+
+const cor = 'vermelho';
+const corPadrao = cor || 'branco'
+
+console.log(corPadrao)
---
+++ basico/operadores/logicos.js
@@ -0,0 +1,18 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || false || false;
+
+// const usuario = 'leonardo'; // form usuário digitou
+// const senha = '123456'; // form usuário digitou
+
+// //                      True                  False
+// const vaiLogar = usuario === 'leonardo' && senha === '12345';
+
+console.log(!!true);
+
+
